cb11017a6810ecd363b09e42a4bda87dfea18230
Marco Boucas <marco.boucas@student-cs.fr>
2018-11-13 15:32:06 +0100

    Finalisation du coverage,la grille est cree et prête à être affichée

--- 2048game/grid_2048.py
+++ 2048game/grid_2048.py
@@ -1,18 +1,33 @@
 from random import randint,random
 
-probability=0.5
+probability = 0.5
+
+
 def create_grid(taille):
+    """
+    Function that generate a grid of size: taille*taille
+    The grid is empty (only ' ')
+    """
     game_grid = []
     for i in range(0,taille):
         game_grid.append([' ',' ',' ', ' '])
     return game_grid
 
 def grid_initialiser(game_grid):
+    """
+    Function that take an empty grid and randomly add 2 elements in it and return the grid
+    """
     game_grid=grid_add_random(game_grid)
     game_grid=grid_add_random(game_grid)
     return game_grid
 
 def grid_add_random(game_grid):
+    """
+    Function that take a grid and add one element (a '2' of a '4' depending on the probability)
+    Only in an empty tile 
+    Return the grid
+    
+    """
     taille=len(game_grid)
     x=randint(0,taille-1)
     y=randint(0,taille-1)
