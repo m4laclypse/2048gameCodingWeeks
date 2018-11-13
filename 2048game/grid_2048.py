2de49bd1bfd9e5f4be2bc6132444f994af1984d1
Marco Boucas <marco.boucas@student-cs.fr>
2018-11-13 15:18:09 +0100

    Ca semble initialiser

--- 2048game/grid_2048.py
+++ 2048game/grid_2048.py
@@ -1,5 +1,32 @@
-def create_grid():
-    s = []
-    for i in range(0,4):
-        s.append([' ',' ',' ', ' '])
-    return s
+from random import randint,random
+
+probability=0.5
+def create_grid(taille):
+    game_grid = []
+    for i in range(0,taille):
+        game_grid.append([' ',' ',' ', ' '])
+    return game_grid
+
+def grid_initialiser(game_grid):
+    game_grid=grid_add_random(game_grid)
+    game_grid=grid_add_random(game_grid)
+    return game_grid
+
+def grid_add_random(game_grid):
+    taille=len(game_grid)
+    x=randint(0,taille-1)
+    y=randint(0,taille-1)
+    while game_grid[x][y]!=' ':
+        x=randint(0,taille-1)
+        y=randint(0,taille-1)
+    if random()>probability:
+        val="2"
+    else:
+        val='4'
+    grid_add_new_tile_at_position(game_grid,x,y,val)
+    return game_grid
+
+
+def grid_add_new_tile_at_position(game_grid,x,y,val):
+    game_grid[x][y]=val
+    return game_grid
--- 2048game/test_grid_2048.py
+++ 2048game/test_grid_2048.py
@@ -1,7 +1,17 @@
-from grid_2048 import create_grid
+from grid_2048 import *
 from pytest import *
 
 
 def test_create_grid():
-    assert create_grid() == [[' ',' ',' ', ' '],[' ',' ',' ', ' '],[' ',' ',' ', ' '],[' ',' ',' ', ' ']]
+    assert create_grid(4) == [[' ',' ',' ', ' '],[' ',' ',' ', ' '],[' ',' ',' ', ' '],[' ',' ',' ', ' ']]
 
+def test_grid_add_new_tile_at_position():
+    game_grid=create_grid(4)
+    game_grid=grid_add_new_tile_at_position(game_grid,1,1,'2')
+    assert game_grid==[[' ',' ',' ', ' '],[' ', '2' ,' ', ' '],[' ',' ',' ', ' '],[' ',' ',' ', ' ']]
+    
+def test_grid_add_random():
+    game_grid=create_grid(4)
+    game_grid=grid_add_random(game_grid)
+    game_grid=grid_add_random(game_grid)
+    assert '2' or '4' in game_grid
