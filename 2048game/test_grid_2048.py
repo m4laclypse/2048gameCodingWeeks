227064cbb9e8a114090782508dfdf20ca9f0e412
Marco Boucas <marco.boucas@student-cs.fr>
2018-11-13 14:53:36 +0100

    J'ai rajouté les deux fichiers de base pour l'affichage de la grille avec pytest

---
+++ 2048game/test_grid_2048.py
@@ -0,0 +1,7 @@
+from grid_2048 import create_grid
+from pytest import *
+
+
+def test_create_grid():
+    assert create_grid() == [[' ',' ',' ', ' '],[' ',' ',' ', ' '],[' ',' ',' ', ' '],[' ',' ',' ', ' ']]
+
